f8076af7ffedbcd346153b54f08cf17b6a0f0334
hemisrt8 <khvorovartem@gmail.com>
2016-06-12 19:02:51 +0300

    Edit unit test of Validator after logic changes
    * Previously validator was throwing exception in case of invalid arguments, logic has been changed to return boolean value instead of throwing an exceptions

--- src/test/java/ua/khvorov/utils/ValidatorTest.java
+++ src/test/java/ua/khvorov/utils/ValidatorTest.java
@@ -1,25 +1,27 @@
 package ua.khvorov.utils;
 
 import org.junit.Test;
-
 import ua.khvorov.lottery.utils.Validator;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class ValidatorTest {
 
     @Test
     public void givenArgsAmountEqualToTwoThenValidatorShouldNotRaiseAnException() {
         String[] args = {"Alison, Alice, Austria, 5555", "1234567890"};
-        Validator.areArgumentsValid(args);
+        assertThat(Validator.areArgumentsValid(args), is(true));
     }
 
-    @Test(expected = RuntimeException.class)
+    @Test
     public void givenArgsAmountNotEqualToTwoThenValidatorShouldRaiseAnException() {
         String[] args = {"Alison, Alice, Austria, 5555"};
-        Validator.areArgumentsValid(args);
+        assertThat(Validator.areArgumentsValid(args), is(false));
     }
 
-    @Test(expected = RuntimeException.class)
+    @Test
     public void givenNullArgsThenValidatorShouldRaiseAnException() {
-        Validator.areArgumentsValid(null);
+        assertThat(Validator.areArgumentsValid(null), is(false));
     }
 }
